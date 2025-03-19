ea5829f5b3676bd447b82598bd87a64096a3b1f9
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2025-03-19 21:23:14 +0100

    nice design

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,11 +3,11 @@
 	let { translations } = data;
 </script>
 
-<div data-sveltekit-preload-data="tap flex flex-col justify-center w-screen h-screen">
-	<div class="m-auto size-96 border-2 rounded">
-		<h1>{translations.login}</h1>
-		<img src='/gh_light.svg' alt='GitHub Logo' class="size-48 hidden dark:block" />
-		<img src='/gh_dark.svg' alt='GitHub Logo' class="size-48 block dark:hidden" />
-		<a href="/login/github">{translations.loginwithgithub}</a>
+<div data-sveltekit-preload-data="tap" class="flex flex-col justify-center h-screen">
+	<div class="m-auto size-96 border-2 rounded flex flex-col justify-evenly">
+		<h1 class="text-2xl font-bold mx-auto">{translations.login}</h1>
+		<img src='/gh_light.svg' alt='GitHub Logo' class="size-32 mx-auto hidden dark:block" />
+		<img src='/gh_dark.svg' alt='GitHub Logo' class="size-32 mx-auto block dark:hidden" />
+		<a href="/login/github" class="dark:border-white border-black border-2 rounded w-fit p-2 mx-auto">{translations.loginwithgithub}</a>
 	</div>
 </div>
