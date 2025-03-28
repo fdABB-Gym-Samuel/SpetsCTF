ac1302a1be5b7a53dba02e697cfe9a9ee5cb8445
Samuel <samuel.olsson@hitachigymnasiet.se>
2025-03-28 22:19:51 +0100

    Last clean up 🧹, i swear, almost

--- src/routes/(wargame_platform)/challenges/+page.server.ts
+++ src/routes/(wargame_platform)/challenges/+page.server.ts
@@ -83,15 +83,11 @@ export const load: PageServerLoad = async ({ locals }: ServerLoadEvent) => {
 				'[]'::json
 			  )
 			`.as('resources'),
-			// Check if the CTF event has ended (current time is after ctf.end_time).
-			sql`(NOW() > ctf.end_time)`.as('ctf_ended')
 		])
-
 		.orderBy('ch.points', 'desc')
 		//   .orderBy('rs.first_time')
 		.execute();
 
-	console.log(challenges[0].ctf_ended);
 
 	return { challenges };
 };
