eceaf38f27917afffc2884934a7ca9cd7076de14
Samuel <samuel.olsson@hitachigymnasiet.se>
2025-04-16 22:36:13 +0200

    Return 404 in layout when ctf isnt found

--- src/routes/(ctf_platform)/ctf/[ctf_id=ctf_id]/+layout.server.ts
+++ src/routes/(ctf_platform)/ctf/[ctf_id=ctf_id]/+layout.server.ts
@@ -1,9 +1,26 @@
 import { db } from '$lib/db/database';
 import type { LayoutServerLoad } from './$types';
+import { error } from '@sveltejs/kit';
 
 export const load: LayoutServerLoad = async ({ locals, depends, params }) => {
 	const user = locals.user;
 	const ctfId = Number(params.ctf_id);
+
+	const ctf_data = await db
+		.selectFrom('ctf_events')
+		.select([
+			'ctf_events.display_name',
+			'ctf_events.start_time',
+			'ctf_events.end_time',
+			'ctf_events.max_team_size'
+		])
+		.where('id', '=', ctfId)
+		.executeTakeFirst();
+	
+	if (!ctf_data) {
+		error(404, { message: 'CTF does not exist.' });
+	}
+
 	let team;
 	let isOrg;
 	if (!user) {
