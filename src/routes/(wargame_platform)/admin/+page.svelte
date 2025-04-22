6da90af396af1d346f97402b846e561bc23f7e33
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2025-04-22 13:49:06 +0200

    add link to admin page

--- src/routes/(wargame_platform)/admin/+page.svelte
+++ src/routes/(wargame_platform)/admin/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	let { data } = $props();
 	let { translations } = data;
+	import { Wrench } from '@lucide/svelte';
 
 	interface adminLink {
 		display: string;
@@ -8,15 +9,17 @@
 	}
 
 	const adminLinks: adminLink[] = [
-		{ href: '/admin/create-challenge', display: translations.createchallenge }
+		{ href: '/admin/create-challenge', display: translations.createchallenge },
+		{ href: '/admin/create-ctf', display: translations.createctf }
 	];
 </script>
 
-<div>
+<div class="content">
 	<h1>{translations.cuttingedgeadmintools}</h1>
-	<ul>
+	<ul class="mt-4 flex flex-col space-y-4">
 		{#each adminLinks as adminLink}
-			<li>
+			<li class="flex flex-row">
+				<Wrench class="mr-3" />
 				<a class="text-accent-dark underline" href={adminLink.href}>
 					{adminLink.display}
 				</a>
