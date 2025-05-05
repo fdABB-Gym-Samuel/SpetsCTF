0b2946e62bb976bb183b4203f1316e8fe6542397
Samuel <samuel.olsson@hitachigymnasiet.se>
2025-05-05 12:22:49 +0200

    Fix minor bugs with create-ctf

--- src/routes/(wargame_platform)/admin/create-ctf/+page.svelte
+++ src/routes/(wargame_platform)/admin/create-ctf/+page.svelte
@@ -21,50 +21,60 @@
 	$inspect(formValid);
 </script>
 
-<div class="w-screen">
+<div class="mt-4 w-screen">
 	<h1>{translations.createctf}</h1>
 	<form method="POST" class="mx-auto flex w-1/2 flex-col space-y-2" use:enhance>
-		<label for="display_name">{translations.ctf_display_name_label}</label>
-		<input
-			type="text"
-			class="border p-1"
-			placeholder={translations.displaynamectf}
-			name="display_name"
-			id="display_name"
-		/>
-		<label for="short_name">{translations.ctf_short_name_label}</label>
-		<input
-			type="text"
-			class="border p-1"
-			placeholder={translations.shortnamectf}
-			name="short_name"
-			id="short_name"
-		/>
-		<label for="max_team_size">{translations.ctf_max_team_size_label}</label>
-		<input
-			type="number"
-			class="border p-1"
-			value={null}
-			placeholder="3"
-			name="max_team_size"
-			id="max_team_size"
-		/>
-		<label for="start_time">{translations.ctf_start_time_label}</label>
-		<input
-			bind:value={start_time}
-			type="datetime-local"
-			class="border p-1"
-			name="start_time"
-			id="start_time"
-		/>
-		<label for="end_time">{translations.ctf_end_time_label}</label>
-		<input
-			bind:value={end_time}
-			type="datetime-local"
-			class="border p-1"
-			name="end_time"
-			id="end_time"
-		/>
+		<div>
+			<label for="display_name">{translations.ctf_display_name_label}</label>
+			<input
+				type="text"
+				class="w-full border p-1"
+				placeholder={translations.displaynamectf}
+				name="display_name"
+				id="display_name"
+			/>
+		</div>
+		<div>
+			<label for="short_name">{translations.ctf_short_name_label}</label>
+			<input
+				type="text"
+				class="w-full border p-1"
+				placeholder={translations.shortnamectf}
+				name="short_name"
+				id="short_name"
+			/>
+		</div>
+		<div>
+			<label for="max_team_size">{translations.ctf_max_team_size_label}</label>
+			<input
+				type="number"
+				class="w-full border p-1"
+				value={null}
+				placeholder="3"
+				name="max_team_size"
+				id="max_team_size"
+			/>
+		</div>
+		<div class="flex flex-col">
+			<label for="start_time">{translations.ctf_start_time_label}</label>
+			<input
+				bind:value={start_time}
+				type="datetime-local"
+				class="border p-1"
+				name="start_time"
+				id="start_time"
+			/>
+		</div>
+		<div class="flex flex-col">
+			<label for="end_time">{translations.ctf_end_time_label}</label>
+			<input
+				bind:value={end_time}
+				type="datetime-local"
+				class="border p-1"
+				name="end_time"
+				id="end_time"
+			/>
+		</div>
 		<button type="submit" disabled={!formValid}>{translations.submit}</button>
 	</form>
 </div>
