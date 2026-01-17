dab0fb3538164c8e7b7f79da1db5bd4748a490ce
Eric Thorburn <hyderix@protonmail.com>
2026-01-17 21:03:36 +0100

    try magic incantation

--- ci/lint.sh
+++ ci/lint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -o pipefail
+
 ERR=0
 
 bun install || ERR=1
@@ -8,7 +10,7 @@ bun run prepare || ERR=1
 
 make ./tmp || ERR=1
 export DATABASE_URL=postgresql://spetsctf@/spetsctf?host=$(readlink ./tmp) || ERR=1
-make postgres || ERR=1
+make postgres >/dev/null || ERR=1
 make codegen || ERR=1
 
 echo "::add-matcher::ci/matchers/svelte.json"
