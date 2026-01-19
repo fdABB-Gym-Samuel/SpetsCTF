1c64bfe0e8b2c202db997aa9f55f0d8aeb2f46b1
Eric Thorburn <eric.thorburn@hitachigymnasiet.se>
2026-01-19 13:46:06 +0100

    new func

--- src/lib/server/credentials.ts
+++ src/lib/server/credentials.ts
@@ -1,7 +1,7 @@
 import { env } from '$env/dynamic/private';
 import { readFileSync } from 'node:fs';
 import { join } from 'node:path';
-import { building } from '$app/environment';
+import { building, dev } from '$app/environment';
 
 function getCredentialsDir(): string {
     if (env.CREDENTIALS_DIRECTORY) {
@@ -12,37 +12,89 @@ function getCredentialsDir(): string {
 }
 
 export function getGithubClientId(): string {
-    if (building) return 'dummy';
-    const credentialsDirectory = getCredentialsDir();
-    if (credentialsDirectory.length > 0) {
-        const contents = readFileSync(join(credentialsDirectory, 'github_client_id'));
-        return contents.toString();
-    } else {
+    if (building) {
+        return 'dummy';
+    } else if (dev) {
         if (env.GITHUB_CLIENT_ID) {
             return env.GITHUB_CLIENT_ID;
         } else {
             throw new Error(
-                'Neither $CREDENTIALS_DIRECTORY/github_client_id nor $GITHUB_CLIENT_ID is set.'
+                'We are in dev and the environment variable $GITHUB_CLIENT_ID is set.'
             );
+        }
+    } else {
+        const credentialsDirectory = getCredentialsDir();
+        if (credentialsDirectory.length > 0) {
+            try {
+                const contents = readFileSync(
+                    join(credentialsDirectory, 'github_client_id')
+                );
+                return contents.toString().trim();
+            } catch {
+                throw new Error(
+                    'Could not read $CREDENTIALS_DIRECTORY/github_client_id.'
+                );
+            }
+        } else {
+            throw new Error('We are in prod and $CREDENTIALS_DIRECTORY is empty.');
         }
     }
 }
 
 export function getGithubClientSecret(): string {
-    if (building) return 'dummy';
-    const credentialsDirectory = getCredentialsDir();
-    if (credentialsDirectory.length > 0) {
-        const contents = readFileSync(
-            join(credentialsDirectory, 'github_client_secret')
-        );
-        return contents.toString();
-    } else {
+    if (building) {
+        return 'dummy';
+    } else if (dev) {
         if (env.GITHUB_CLIENT_SECRET) {
             return env.GITHUB_CLIENT_SECRET;
         } else {
             throw new Error(
-                'Neither $CREDENTIALS_DIRECTORY/github_client_secret nor $GITHUB_CLIENT_SECRET is set.'
+                'We are in dev and the environment variable $GITHUB_CLIENT_SECRET is set.'
             );
+        }
+    } else {
+        const credentialsDirectory = getCredentialsDir();
+        if (credentialsDirectory.length > 0) {
+            try {
+                const contents = readFileSync(
+                    join(credentialsDirectory, 'github_client_secret')
+                );
+                return contents.toString().trim();
+            } catch {
+                throw new Error(
+                    'Could not read $CREDENTIALS_DIRECTORY/github_client_secret.'
+                );
+            }
+        } else {
+            throw new Error('We are in prod and $CREDENTIALS_DIRECTORY is empty.');
+        }
+    }
+}
+
+export function getPostgresConnectionString(): string {
+    if (building) {
+        return 'dummy';
+    } else if (dev) {
+        if (env.DATABASE_URL) {
+            return env.DATABASE_URL;
+        } else {
+            throw new Error(
+                'We are in dev and the environment variable $DATABASE_URL is set.'
+            );
+        }
+    } else {
+        const credentialsDirectory = getCredentialsDir();
+        if (credentialsDirectory.length > 0) {
+            try {
+                const contents = readFileSync(
+                    join(credentialsDirectory, 'database_url')
+                );
+                return contents.toString().trim();
+            } catch {
+                throw new Error('Could not read $CREDENTIALS_DIRECTORY/database_url.');
+            }
+        } else {
+            throw new Error('We are in prod and $CREDENTIALS_DIRECTORY is empty.');
         }
     }
 }
