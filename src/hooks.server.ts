5a929516f3ad37b4cc53fdd0a63ab2c06670f275
Eric Thorburn <hyderix@protonmail.com>
2026-01-19 21:04:18 +0100

    try logging to find the issue

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -13,6 +13,31 @@ export const handle: Handle = async ({ event, resolve }) => {
         event.request.headers.get('accept-language') ?? 'en-US'
     );
 
+    if (event.request.method === 'POST') {
+        console.log('=== POST Request Debug ===');
+        console.log('URL:', event.url.href);
+        console.log('URL Origin:', event.url.origin);
+        console.log('URL Protocol:', event.url.protocol);
+        console.log('URL Host:', event.url.host);
+
+        console.log('\n--- Headers ---');
+        console.log('Origin Header:', event.request.headers.get('origin'));
+        console.log(
+            'X-Forwarded-Proto:',
+            event.request.headers.get('x-forwarded-proto')
+        );
+        console.log('X-Forwarded-Host:', event.request.headers.get('x-forwarded-host'));
+        console.log('X-Forwarded-For:', event.request.headers.get('x-forwarded-for'));
+        console.log('Host:', event.request.headers.get('host'));
+        console.log('Content-Type:', event.request.headers.get('content-type'));
+
+        console.log('\n--- All Headers ---');
+        event.request.headers.forEach((value, key) => {
+            console.log(`${key}: ${value}`);
+        });
+        console.log('======================\n');
+    }
+
     if (token === null) {
         event.locals.user = undefined;
         event.locals.session = undefined;
