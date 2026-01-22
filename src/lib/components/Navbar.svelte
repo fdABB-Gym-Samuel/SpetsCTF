f8edb7d6bc67617a8537489f8e3fa4b5669ca94a
Hannes <hannes.gingby@hitachigymnasiet.se>
2026-01-22 12:59:53 +0100

    fix challenge dialog and mobile nav

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,7 +2,6 @@
     import { goto } from '$app/navigation';
     import type { Users } from '$lib/generated/db';
     import type { Selectable } from 'kysely';
-    import { Menu } from '@lucide/svelte';
 
     import { enhance } from '$app/forms';
     import VSeperator from './VSeperator.svelte';
@@ -13,8 +12,10 @@
 
     import Button from './Button.svelte';
 
-    import { ArrowLeft, LogIn } from '@lucide/svelte';
+    import { LogIn } from '@lucide/svelte';
     import IconSignInBold from 'phosphor-icons-svelte/IconSignInBold.svelte';
+    import IconListBold from 'phosphor-icons-svelte/IconListBold.svelte';
+    import IconXBold from 'phosphor-icons-svelte/IconXBold.svelte';
 
     interface NavLink {
         display: string;
@@ -98,7 +99,19 @@
 
 <nav
     class="bg-bg-900 fixed top-0 left-0 z-50 flex h-15 w-full flex-row items-center justify-between gap-5 px-3 pt-1 *:w-1/3 lg:px-8 xl:px-16">
-    <div class="flex w-1/5 grow-0 flex-row items-center sm:grow">
+    <div class="flex w-1/5 grow-0 flex-row items-center gap-x-2 sm:grow">
+        <div class="block {links.length > 3 ? 'md:hidden' : 'sm:hidden'}">
+            <button
+                class="ignore-default ml-1 flex flex-col"
+                aria-label="Toggle Sidebar"
+                onclick={toggleSidebar}>
+                {#if !showSidebar}
+                    <IconListBold class="text-text-150 text-[24px]" />
+                {:else}
+                    <IconXBold class="text-text-150 text-[24px]" />
+                {/if}
+            </button>
+        </div>
         <div class="logo-continer relative flex justify-end sm:mr-0">
             <svg
                 viewBox="0 0 180 100"
@@ -136,14 +149,6 @@
                     alt="SpetsCTF"
                     class="logo h-5 min-w-25 select-none" />
             </a>
-        </div>
-        <div class="mr-4 block {links.length > 3 ? 'md:hidden' : 'sm:hidden'}">
-            <button
-                class="ignore-default ml-2 flex flex-col gap-1.5"
-                aria-label="Open Sidebar"
-                onclick={toggleSidebar}>
-                <Menu></Menu>
-            </button>
         </div>
     </div>
     <ul
@@ -241,58 +246,22 @@
         role="button"
         tabindex="0">
         <nav
-            class="bg-bg-900 border-r-primary bottom-0 z-40 flex h-full w-120 max-w-9/10 flex-col justify-between border-r-2 px-4 pb-8">
-            <div>
-                <div class="">
-                    <button
-                        aria-label="Close sidebar"
-                        class="ignore-default"
-                        onclick={toggleSidebar}>
-                        <ArrowLeft />
-                    </button>
-                </div>
-                <div class="w-full px-2">
-                    <ul class="flex h-full flex-col items-stretch space-x-2 pr-5 pl-2">
-                        {#each links as link (link.href)}
-                            <li class="border-primary-light m-0 border-b-2 py-2 pl-1">
-                                <a
-                                    class="ignore-default hover:text-primary!"
-                                    rel="external"
-                                    href={link.href}
-                                    onclick={(e) => {
-                                        toggleSidebar(e, false);
-                                    }}>{link.display}</a>
-                            </li>
-                        {/each}
-                    </ul>
-                </div>
-            </div>
-            <div class="px-4">
-                {#if user}
-                    <a
-                        href={resolve('/user')}
-                        onclick={(e) => {
-                            toggleSidebar(e, false);
-                        }}
-                        class="ignore-default max-w-full space-x-4 truncate text-center underline">
-                        <!-- <User class=" inline-block min-h-6 min-w-6" /> -->
-                        <img
-                            src={`https://avatars.githubusercontent.com/u/${user.github_id}?v=4`}
-                            alt="User Avatar"
-                            class="mx-1 inline-block h-6 w-6 rounded-full align-middle" />
-                        {user.display_name || user.github_username}</a>
-                {:else}
-                    <Button
-                        label={translations.login}
-                        type="button"
-                        responsiveStyles="!px-6 md:!px-8"
-                        onclick={(e: MouseEvent) => {
-                            goto(resolve('/login'));
-                            toggleSidebar(e, false);
-                        }}
-                        Icon={LogIn}
-                        aria-label="Login" />
-                {/if}
+            class="bg-bg-900 bottom-0 z-40 flex h-full w-full max-w-4/5 flex-col justify-between px-4 pb-8">
+            <div class="mt-12 w-full px-2">
+                <ul
+                    class="flex h-full flex-col items-stretch gap-3 space-x-2 pr-5 pl-2">
+                    {#each links as link (link.href)}
+                        <li class="border-primary-light m-0 border-b-2 py-4 pl-1">
+                            <a
+                                class="ignore-default hover:text-text-150"
+                                rel="external"
+                                href={link.href}
+                                onclick={(e) => {
+                                    toggleSidebar(e, false);
+                                }}>{link.display}</a>
+                        </li>
+                    {/each}
+                </ul>
             </div>
         </nav>
     </div>
